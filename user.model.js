67973112d71985018d88793f99c6f86b4308025f
MaksItKotik <70341920+MaksItKotik@users.noreply.github.com>
2021-12-25 19:40:54 +0200

    Update user.model.js

--- user.model.js
+++ user.model.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
+// If you wanna you can replace this line on:
+//      import import mongoose from 'mongoose';
+//      const { Schema } = mongoose;
 
+// If you use IMPORT on first comment set:
+//      ... = new Schema({...
 const user = mongoose.Schema({
     time: {type: 'string', required: true},
     name: {type: 'string', required: true},
@@ -9,4 +14,6 @@ const user = mongoose.Schema({
     image: {type: 'string', required: true}
 });
 
+// If you use IMPORT set replace bottom line to:
+//      export default mongoose.model(...)
 module.exports = mongoose.model('Users', user);
